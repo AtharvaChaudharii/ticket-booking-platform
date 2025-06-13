8db6ab648d8daa33c8f6c31d71f2830dabec33d5
Atharva Chaudhari <151078284+CodeAtharva12@users.noreply.github.com>
2025-06-13 16:05:07 +0530

    eventsLists completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import EventList from "@/components/EventList";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
+      <EventList/>
     </div>
   );
 }
